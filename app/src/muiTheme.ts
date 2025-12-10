16c1b172770af56a2d04a7b550756a7b5308b37f
Thomas Geissl <thomas.geissl@gmail.com>
2025-12-10 15:03:04 +0100

    fixed theme

--- app/src/muiTheme.ts
+++ app/src/muiTheme.ts
@@ -56,7 +56,7 @@ const theme = createTheme({
     borderRadius: 14,
   },
 
-  shadows: Array(25).fill("0 4px 14px rgba(0, 0, 0, 0.32)"),
+  // shadows: Array(25).fill("0 4px 14px rgba(0, 0, 0, 0.32)"),
 
   components: {
     MuiCssBaseline: {
