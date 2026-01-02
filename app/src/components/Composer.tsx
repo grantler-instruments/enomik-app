f399fa7bf2c2a6e92a1c5ad03d3f4faa0e241b4b
Thomas Geissl <thomas.geissl@gmail.com>
2026-01-02 15:53:07 +0100

    some responsive fixes

--- app/src/components/Composer.tsx
+++ app/src/components/Composer.tsx
@@ -5,6 +5,7 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Stack,
   TextField,
 } from "@mui/material";
 import { Send } from "@mui/icons-material";
@@ -13,6 +14,7 @@ import { useMIDIStore, type MidiMessage } from "../store/midi";
 import { v4 as uuidv4 } from "uuid";
 import { MIDI_STATUS, typeToLabel } from "../utils/midi";
 import MidiDeviceChooser from "./MidiDeviceChooser";
+
 const Composer = () => {
   const sendMessage = useMIDIStore((state) => state.sendMessage);
   const selectedOutputId = useMIDIStore(
@@ -21,6 +23,7 @@ const Composer = () => {
   const setSelectedOutputId = useMIDIStore(
     (state) => state.setSelectedComposerOutputDevice
   );
+
   const [channel, setChannel] = useState(1);
   const [type, setType] = useState(144);
   const [noteOrCc, setNoteOrCc] = useState(60);
@@ -28,6 +31,7 @@ const Composer = () => {
   const [pitchBendValue, setPitchBendValue] = useState(0);
   const [sysexData, setSysexData] = useState("");
   const [manufacturerId, setManufacturerId] = useState("");
+
   const types = [
     { value: MIDI_STATUS.NOTE_ON, label: typeToLabel(MIDI_STATUS.NOTE_ON) },
     { value: MIDI_STATUS.NOTE_OFF, label: typeToLabel(MIDI_STATUS.NOTE_OFF) },
@@ -60,29 +64,34 @@ const Composer = () => {
     { value: MIDI_STATUS.CONTINUE, label: typeToLabel(MIDI_STATUS.CONTINUE) },
   ];
 
-  //TODO: move to utils
   const hexStringToArray = (hex: string): number[] => {
-    // Remove all whitespace
     const clean = hex.replace(/\s+/g, "").toUpperCase();
     const result: number[] = [];
 
-    // Parse every two characters as a hex byte
     for (let i = 0; i < clean.length; i += 2) {
       result.push(parseInt(clean.slice(i, i + 2), 16));
     }
 
     return result;
   };
+
   return (
-    <Box display={"flex"} flexDirection={"column"} gap={2}>
-      <Box display="flex" flexDirection="row" gap={2}>
-        <FormControl sx={{ width: 120 }}>
+    <Box>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={2}
+        alignItems={{ sm: "center" }}
+        width="100%"
+      >
+        <FormControl
+          size="small"
+          sx={{ width: { xs: "100%", sm: 120 } }}
+        >
           <InputLabel>Channel</InputLabel>
           <Select
             value={channel}
             label="Channel"
             onChange={(e) => setChannel(Number(e.target.value))}
-            size="small"
           >
             {[...Array(16)].map((_, i) => (
               <MenuItem key={i + 1} value={i + 1}>
@@ -91,13 +100,16 @@ const Composer = () => {
             ))}
           </Select>
         </FormControl>
-        <FormControl sx={{ minWidth: 150 }}>
+
+        <FormControl
+          size="small"
+          sx={{ width: { xs: "100%", sm: 180 } }}
+        >
           <InputLabel>Type</InputLabel>
           <Select
             value={type}
             label="Type"
-            onChange={(e) => setType(e.target.value)}
-            size="small"
+            onChange={(e) => setType(Number(e.target.value))}
           >
             {types.map((t) => (
               <MenuItem key={t.value} value={t.value}>
@@ -106,40 +118,42 @@ const Composer = () => {
             ))}
           </Select>
         </FormControl>
+
         {type === MIDI_STATUS.SYSEX_START && (
           <>
             <TextField
               label="Start Byte"
               value="F0"
               disabled
-              sx={{ width: 80 }}
               size="small"
+              sx={{ width: { xs: "100%", sm: 80 } }}
             />
             <TextField
               label="Manufacturer ID (*)"
               value={manufacturerId}
               onChange={(e) => setManufacturerId(e.target.value)}
               placeholder="FD"
-              sx={{ width: 120 }}
               size="small"
+              sx={{ width: { xs: "100%", sm: 140 } }}
             />
             <TextField
               label="Data (hex)"
               value={sysexData.replace(/\s+/g, "").toUpperCase()}
               onChange={(e) => setSysexData(e.target.value)}
               placeholder="43 12 00"
-              sx={{ minWidth: 200 }}
               size="small"
+              sx={{ width: { xs: "100%", sm: 240 } }}
             />
             <TextField
               label="End Byte"
               value="F7"
               disabled
-              sx={{ width: 80 }}
               size="small"
+              sx={{ width: { xs: "100%", sm: 80 } }}
             />
           </>
         )}
+
         {(type === MIDI_STATUS.NOTE_ON ||
           type === MIDI_STATUS.NOTE_OFF ||
           type === MIDI_STATUS.CHANNEL_PRESSURE ||
@@ -161,59 +175,65 @@ const Composer = () => {
                 type="number"
                 value={noteOrCc}
                 onChange={(e) => setNoteOrCc(Number(e.target.value))}
-                sx={{ width: 100 }}
                 size="small"
+                sx={{ width: { xs: "100%", sm: 120 } }}
               />
             )}
-            {type !== 192 && (
+
+            {type !== MIDI_STATUS.PROGRAM_CHANGE && (
               <TextField
                 label={
-                  type === MIDI_STATUS.NOTE_ON || type === MIDI_STATUS.NOTE_OFF
+                  type === MIDI_STATUS.NOTE_ON ||
+                  type === MIDI_STATUS.NOTE_OFF
                     ? "Velocity"
                     : "Value"
                 }
                 type="number"
                 value={velocityOrValue}
                 onChange={(e) => setVelocityOrValue(Number(e.target.value))}
-                sx={{ width: 100 }}
                 size="small"
+                sx={{ width: { xs: "100%", sm: 120 } }}
               />
             )}
           </>
         )}
+
         {type === MIDI_STATUS.PITCH_BEND && (
           <TextField
-            label="Pitch Bend Value (-8192 to +8191)"
+            label="Pitch Bend (-8192 to +8191)"
             type="number"
             value={pitchBendValue}
             onChange={(e) => setPitchBendValue(Number(e.target.value))}
             size="small"
+            sx={{ width: { xs: "100%", sm: 220 } }}
           />
         )}
-        <Box flex={1}></Box>
+
+        <Box sx={{ flexGrow: { sm: 1 } }} />
+
         <MidiDeviceChooser
           value={selectedOutputId || ""}
           onChange={setSelectedOutputId}
         />
+
         <Button
           variant="contained"
           startIcon={<Send />}
+          sx={{ width: { xs: "100%", sm: "auto" } }}
           onClick={() => {
             const msg: MidiMessage = {
               id: uuidv4(),
-              type: type,
+              type,
               timestamp: Date.now(),
-              channel: channel,
+              channel,
               note: type !== 240 ? noteOrCc : undefined,
               controller: type !== 240 ? noteOrCc : undefined,
               value:
-                type === 240
-                  ? undefined
-                  : type === MIDI_STATUS.CONTROL_CHANGE ||
-                    type === MIDI_STATUS.NOTE_ON ||
-                    type === MIDI_STATUS.NOTE_OFF || 
-                    type === MIDI_STATUS.CHANNEL_PRESSURE ||
-                    type === MIDI_STATUS.POLY_PRESSURE  
+                type === MIDI_STATUS.CONTROL_CHANGE ||
+                type === MIDI_STATUS.NOTE_ON ||
+                type === MIDI_STATUS.NOTE_OFF ||
+                type === MIDI_STATUS.CHANNEL_PRESSURE ||
+                type === MIDI_STATUS.POLY_PRESSURE
                   ? velocityOrValue
                   : undefined,
               pitchBendValue:
@@ -222,22 +242,26 @@ const Composer = () => {
                   : undefined,
               velocity: type !== 240 ? velocityOrValue : undefined,
               data:
-                type === 240
+                type === MIDI_STATUS.SYSEX_START
                   ? [
                       0xf0,
-                      ...(manufacturerId ? [parseInt(manufacturerId, 16)] : []),
+                      ...(manufacturerId
+                        ? [parseInt(manufacturerId, 16)]
+                        : []),
                       ...hexStringToArray(sysexData),
                       0xf7,
                     ]
                   : undefined,
             };
+
             sendMessage(msg, selectedOutputId);
           }}
         >
           Send MIDI
         </Button>
-      </Box>
+      </Stack>
     </Box>
   );
 };
+
 export default Composer;
